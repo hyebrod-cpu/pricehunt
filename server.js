90b905ca0866f410820ce251a2ba635e2591f647
hyebrod-cpu <hyebrod@gmail.com>
2026-06-12 13:17:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -103,6 +103,9 @@ app.get('/api/compare', async (req, res) => {
     });
   }
 });
+app.get('/', (req, res) => {
+  res.send('PriceHunt API is running! Use /api/compare?q=your_search_term to search.');
+});
 
 app.listen(PORT, () => {
   console.log(`PriceHunt backend server running on port ${PORT}`);
